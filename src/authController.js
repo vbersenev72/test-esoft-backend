b264da30df39a2c019a97a8e76f1759dc7e7757a
vbersenev72 <vbersenev72@gmail.com>
2023-04-16 14:42:34 +0500

    auth

--- src/authController.js
+++ src/authController.js
@@ -1,10 +1,24 @@
 const Task = require('../models/Task');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {validationResult} = require('express-validator')
+const {secret} = require('./config')
+
+
+const generateAccessToken = (id) => {
+    const payload = {id}
+    return jwt.sign(payload, secret, {expiresIn: "24h"})
+}
 
 class authController {
-    async registration(req, res) {
+    async registration(req, res) { // регистрирует ползователя
         try {
+            const errors = validationResult(req.body) // Валидируем ошибки в запросе
+            if (!errors.isEmpty()) { // Если есть то =>
+                return res.status(400).json({message: "Ошибка при регистрации", error: errors})
+            }
+
             const {name, lastname, username, password} = req.body
             const candidate = await User.findOne({username}) // попытка найти пользователя с таким юзернеймом
 
@@ -16,7 +30,7 @@ class authController {
             const user = new User({name, lastname, username, password: hashPassword}) // создали пользователя в соответствии со схемой
 
             await user.save() // сохранили пользователя в бд
-            res.json({message: 'Пользователь успешно зарегистрирован'})
+            res.json({message: 'Пользователь успешно зарегистрирован', response: user})
 
         } catch (error) {
             console.log(error);
@@ -24,8 +38,23 @@ class authController {
         }
     }
 
-    async login(req, res) {
+    async login(req, res) { // возвращает jwt-токен
         try {
+
+            const {username, password} = req.body
+            const candidate = await User.findOne({username}) // ищем юзера в базе
+            if (!candidate) { // если не нашли
+                return res.status(400).json({message: `Пользователь ${candidate} не найден`})
+            }
+           
+            const validPassword = bcrypt.compareSync(password, candidate.password) // сравниваем введенный пароль с хешем внутри базы данных
+           
+            if (!validPassword) {
+                return res.status(400).json({message: "Пароль неверный"}) //если пароль неверный
+            }
+
+            const token = generateAccessToken(candidate._id) // токен содержит в себе id юзера
+            return res.json({token}) 
 
         } catch (error) {
             console.log(error);
@@ -35,12 +64,24 @@ class authController {
 
     async getUsers(req, res) {
         try {
+            const users = await User.find()
+            res.json(users)
 
-
-            res.json('server work')
         } catch (error) {
             console.log(error);
             res.status(400).json({message: 'error'})
+        }
+    }
+
+    async createTask(req, res) {
+        try {
+            const {title, des, dateFinish, dateCreate, datePut, priority, status, creator} = req.body
+            const task = new Task({title, des, dateFinish, dateCreate, datePut, priority, status, creator})
+
+            await task.save()
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({message: 'create task error'})
         }
     }
 }
--- src/authRouter.js
+++ src/authRouter.js
@@ -1,9 +1,20 @@
 const Router = require('express')
 const router = new Router()
 const authController = require('./authController')
+const {check} = require('express-validator')
 
-router.post('/registration', authController.registration)
-router.post('/login', authController.login)
+router.post('/registration',[
+		check('username', 'Имя пользователя не может быть пустым').notEmpty(),
+		check('password', 'Пароль пользователя не может быть меньше 6 символов').isLength({min: 6, max: 60}),
+	], authController.registration)
+
+router.post('/login',[
+		check('username', 'Имя пользователя не может быть пустым').notEmpty(),
+		check('password', 'Пароль пользователя не может быть меньше 6 символов').isLength({min: 6, max: 60}),
+	], authController.login)
+
+router.post('/create', authController.createTask)
+
 router.post('/users', authController.getUsers)
 
 module.exports = router
